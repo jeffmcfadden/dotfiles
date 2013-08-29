f347b26c8ee6bd0d0a5797fb4457200d056fe3f3
jeffmcfadden <jeff@forgeapps.com>
2013-08-29 13:22:10 -0700

    Some tweaks for myself.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,8 @@
 alias reload!='. ~/.zshrc'
+# I like colors in my ls
+alias ls="ls -laFG"
+
+# #Some git 'shortcuts'
+alias gfprd="git fetch && git pull --rebase origin develop"
+alias gpod="git push origin develop"
+alias gf="git fetch"
